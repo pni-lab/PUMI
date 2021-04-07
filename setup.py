2e97f07da76e10dd1586b2a0d0ac103a8efbb345
Kevin Hoffschlag <72939508+khoffschlag@users.noreply.github.com>
2021-04-07 18:11:38 +0200

    set up versioneer

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+import versioneer
+
+with open("README.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PUMI",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="Laboratory of Predictive Neuroimaging - University Hospital Essen, Germany",
+    description="Nipype-based neuroimaging pipelines with a modular architecture.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
