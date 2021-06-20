18d9f8582f2a852d08f36878b2a567f601bc7465
Kevin Hoffschlag <72939508+khoffschlag@users.noreply.github.com>
2021-06-20 15:59:24 +0200

    more consistent terms

--- PUMI/pipelines/anat/anat2mni.py
+++ PUMI/pipelines/anat/anat2mni.py
@@ -15,13 +15,13 @@ def qc(wf, image_width=500, threshold_edges=0.1):
     wf.connect('inputspec', 'in_file', qc_wf, 'bg_image')
 
 
-@AnatPipeline(inputspec_fields=['brain', 'skull'],
+@AnatPipeline(inputspec_fields=['brain', 'head'],
               outputspec_fields=['output_brain', 'linear_xfm', 'inv_linear_xfm', 'nonlinear_xfm', 'inv_nonlinear_xfm',
                                  'std_template'])
 def anat2mni_fsl(wf, **kwargs):
     # linear registration
     linear_reg = Node(interface=fsl.FLIRT(), name='linear_reg')
-    linear_reg.inputs.cost = 'corratio'  # todo: why this cost function??
+    linear_reg.inputs.cost = 'corratio'
     linear_reg.inputs.reference = get_reference(wf, 'brain')
     wf.connect('inputspec', 'brain', linear_reg, 'in_file')
 
@@ -37,7 +37,7 @@ def anat2mni_fsl(wf, **kwargs):
     nonlinear_reg.inputs.fieldcoeff_file = True
     nonlinear_reg.inputs.jacobian_file = True
     nonlinear_reg.config_file = wf.cfg_parser.get('FSL', 'fnirt_config')
-    wf.connect('inputspec', 'skull', nonlinear_reg, 'in_file')
+    wf.connect('inputspec', 'head', nonlinear_reg, 'in_file')
     wf.connect(linear_reg, 'out_matrix_file', nonlinear_reg, 'affine_file')
 
     # calculate inverse of the fnirt transformation matrix
@@ -67,4 +67,3 @@ def anat2mni_fsl(wf, **kwargs):
     wf.connect(nonlinear_reg, 'field_file', 'outputspec', 'field_file')
     wf.connect(inv_nonlinear_reg, 'inverse_warp', 'outputspec', 'inv_nonlinear_xfm')
     wf.connect(brain_warp, 'out_file', 'outputspec', 'output_brain')
-
