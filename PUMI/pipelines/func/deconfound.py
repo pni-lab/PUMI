32d08c0492efc4a7a00487aa35235609187c2bd9
Mohamad Abouras <mhmad.abouras2222@gmail.com>
2022-01-17 13:40:29 +0100

    - removed not needed sinker in deconfound.py - changed the MRI sequence from 'T1w' to 'bold'

--- PUMI/pipelines/func/deconfound.py
+++ PUMI/pipelines/func/deconfound.py
@@ -3,6 +3,8 @@ from PUMI.engine import NestedNode as Node
 from PUMI.engine import FuncPipeline
 
 
+# Inputspec  is the input of the workflow
+# Outputspec is the output of the workflow
 
 @FuncPipeline(inputspec_fields=['in_file'],
               outputspec_fields=['out_file'])
@@ -10,7 +12,6 @@ def despiking_afni(wf, **kwargs):
     despike = Node(interface=afni.Despike(), name='despike')
     despike.inputs.outputtype = 'NIFTI_GZ'
     wf.connect('inputspec', 'in_file', despike, 'in_file')
-    wf.connect(despike, 'out_file', 'sinker', 'out_file')
     wf.connect(despike, 'out_file', 'outputspec', 'out_file')
 
 
