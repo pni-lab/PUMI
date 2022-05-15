f98d892c0d0c6d51a7c306ebc2aa27e3e366d7d7
Mohamad Abouras <mhmad.abouras2222@gmail.com>
2022-05-16 00:35:22 +0200

    - Paths has to be entered now as cmd arguments - Deleted the use of definition file - Fixed Docstrings

--- examples/bet_subworkflow_func.py
+++ examples/bet_subworkflow_func.py
@@ -6,24 +6,37 @@ from nipype.interfaces import BIDSDataGrabber
 from nipype.utils.filemanip import list_to_filename
 from PUMI.pipelines.anat.segmentation import bet_fsl
 from pipelines.multimodal import ImgExtraction
-
+import argparse
 import os
-
-
 
 
 '''
 Using command line arguments, one can set the paths to the input/output/working directory of the workflow.
-If nothing the arguments were not entered, the program will terminate.
+Remember to enter a full path.
 
-** Make sure to enter the full path.
+If nothing was given, default paths will be used.(Might cause Exception)
 '''
-if len(sys.argv) == 4:
-    input_dir = sys.argv[1]  # Path to the bids
-    output_dir = sys.argv[2]  # End Results of the workflow
-    working_dir = sys.argv[3]  # Path, where Workflow Data will be stored
-else:
-    sys.exit("No cmd arguments were given ")
+
+
+ROOT_DIR = os.path.dirname(os.getcwd())
+input_dir = os.path.join(ROOT_DIR, 'data_in/bids')  # path where the bids data is located
+output_dir = os.path.join(ROOT_DIR,
+                          'data_out')  # path where the folder 'BET' will be created for the results of this script
+working_dir = os.path.join(ROOT_DIR,
+                           'data_out')  # path where the folder 'bet_iter_wf' will be created for the workflow
+
+# Create command line parser in case user wanted to specify the paths.
+# In case user didn't enter arguments, the above defined arguments will be used.
+parser = argparse.ArgumentParser(description='Using command line arguments, one can optionally set the paths to the '
+                                             'input/output/working directory of the workflow.')
+parser.add_argument('-input_dir', nargs='?', metavar='input_dir', type=str, default=input_dir,
+                    help='Path to Bids-Directory path')
+parser.add_argument('-output_dir', nargs='?', metavar='output_dir', type=str, default=output_dir,
+                    help='Path to output directory')
+parser.add_argument('-working_dir', nargs='?', metavar='working_dir', type=str, default=working_dir,
+                    help='Path, where Workflow Data will '
+                         'be stored')
+args = parser.parse_args()
 
 # subjects for which a brain extraction should be performed
 # It needs to be done one after another since images need to be extracted at run time
@@ -33,7 +46,7 @@ else:
 subjects = ['003']
 
 wf = Workflow(name='bet_func_wf')
-wf.base_dir = os.path.abspath(working_dir)
+wf.base_dir = os.path.abspath(args.working_dir)
 
 # create a subroutine (subgraph) for every subject
 inputspec = Node(IdentityInterface(fields=['subject']), name='input_node')
@@ -41,7 +54,7 @@ inputspec.iterables = [('subject', subjects)]
 
 # get functional images
 bids_grabber = Node(BIDSDataGrabber(), name='bids_grabber')
-bids_grabber.inputs.base_dir = os.path.abspath(input_dir)
+bids_grabber.inputs.base_dir = os.path.abspath(args.input_dir)
 bids_grabber.inputs.output_query = {
     'bold': dict(
         subject=subjects,
@@ -67,7 +80,7 @@ wf.connect(bids_grabber, 'bold', path_extractor, 'filelist')
 # Notice that we are using a sub-wf, and that's why we use (inputspec/outputspec) to (enter/get) data
 # Extract 3D Images from 4D Images
 img_extraction_wf = ImgExtraction.img_extraction_workflow(wf_name="img_extraction_wf",
-                                                          sink_tag='sub-003', sink_dir=output_dir, volume='middle')
+                                                          sink_tag='sub-003', sink_dir=args.output_dir, volume='middle')
 wf.connect(path_extractor, 'out_file', img_extraction_wf, 'inputspec.func')
 
 # Do the brain extraction
