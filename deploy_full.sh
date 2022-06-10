9e0d69c976bbc26233c8979d38fbc2e87a3604e5
Tamas Spisak <tamas.spisak@uk-essen.de>
2022-06-10 18:18:29 +0200

    requirements updated

--- deploy_full.sh
+++ deploy_full.sh
@@ -6,17 +6,17 @@ neurodocker generate docker \
     --afni method=binaries version=latest \
     --fsl version=6.0.4 \
     --ants version=2.3.4 \
-    --copy data_in/pumi-minitest /PUMI/data_in/pumi-minitest \
-    --copy data_in/std /PUMI/data_in/std \
-    --copy PUMI /PUMI/PUMI \
-    --copy tests /PUMI/tests \
-    --copy pipelines /PUMI/pipelines \
-    --copy pyproject.toml /PUMI/. \
-    --copy requirements.txt /PUMI/. \
-    --copy MANIFEST.in /PUMI/. \
-    --copy README.md /PUMI/. \
-    --run "mkdir /PUMI/data_out" \
-    --miniconda version=latest pip_install='git+https://github.com/MIC-DKFZ/HD-BET' \
+#    --copy data_in/pumi-minitest /PUMI/data_in/pumi-minitest \
+#    --copy data_in/std /PUMI/data_in/std \
+#    --copy PUMI /PUMI/PUMI \
+#    --copy tests /PUMI/tests \
+#    --copy pipelines /PUMI/pipelines \
+#    --copy pyproject.toml /PUMI/. \
+#    --copy requirements.txt /PUMI/. \
+#    --copy MANIFEST.in /PUMI/. \
+#    --copy README.md /PUMI/. \
+    --run "mkdir -p /PUMI/data_out" \
+    --miniconda version=latest pip_install='graphviz numpydoc nbsphinx dot2tex git+https://github.com/MIC-DKFZ/HD-BET' \
     --yes \
     > pumi.Dockerfile
 
