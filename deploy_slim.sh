1ef4a8075f5900b52dfe20f4fa2389ac226da619
Tamas Spisak <tamas.spisak@uk-essen.de>
2022-06-11 17:15:00 +0200

    patched

--- deploy_slim.sh
+++ deploy_slim.sh
@@ -1,4 +1,4 @@
-VERSION=$1 #`git describe --tags`
+VERSION=$1 #git describe --tags
 echo "Current version (full): $VERSION"
 #VER=`echo $VERSION | cut -d '.' -f 1,2` # major.minor only
 VER=`echo $VERSION | awk -F'[.-]' '{print $1 "." $2 "." $3}'`
@@ -14,19 +14,11 @@ docker run --rm -itd --name pumi-container pnilab/pumi:$V
 echo "* Minify container by running all tests..."
 
 # put fresh pumi code into the container
-docker cp data_in/std pumi-container:/PUMI/data_in/std
-docker cp PUMI pumi-container:/PUMI/PUMI
-docker cp tests pumi-container:/PUMI/tests
-docker cp examples pumi-container:/PUMI/examples
-docker cp pipelines pumi-container:/PUMI/pipelines
-docker cp pyproject.toml pumi-container:/PUMI/pyproject.toml
-docker cp requirements.txt pumi-container:/PUMI/requirements.txt
-docker cp MANIFEST.in pumi-container:/PUMI/MANIFEST.in
-docker cp README.md  pumi-container:/PUMI/README.md
-docker exec pumi-container pip install PUMI
+docker exec mkdir /PUMI/data_in
+docker cp . pumi-container:/PUMI/.
+docker exec pumi-container cd PUMI; poetry install
 
 # back up fsldata
-
 docker exec pumi-container mv /opt/fsl-6.0.4/data/standard /tmp/standard
 
 # cmd="python3 /PUMI/tests/test_afni.py; python3 /PUMI/tests/test_fsl.py; python3 /PUMI/tests/test_ants.py"
@@ -39,6 +31,7 @@ neurodocker minify \
 
 # put back FSL data
 docker exec pumi-container mv /tmp/standard /opt/fsl-6.0.4/data/standard
+
 # create a new Docker image using the pruned container.
 docker export pumi-container | docker import - pnilab/pumi-slim:latest
 docker tag pnilab/pumi-slim:latest pnilab/pumi-slim:$VER  #minor.major.patch
