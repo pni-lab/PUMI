91ec9220d0211ff19f93341bbc30aac244bff8fe
Kevin Hoffschlag <72939508+khoffschlag@users.noreply.github.com>
2022-08-08 13:51:29 +0200

    port nuissremov_workflow

--- PUMI/pipelines/func/deconfound.py
+++ PUMI/pipelines/func/deconfound.py
@@ -21,6 +21,50 @@ def despiking_afni(wf, **kwargs):
     #todo: qc
 
 
+@FuncPipeline(inputspec_fields=['in_file', 'design_file'],
+              outputspec_fields=['out_file'])
+def nuisance_removal(wf, **kwargs):
+    """
+
+    Perform nuisance removal.
+
+    CAUTION: Name in the old PUMI was nuissremov_workflow
+
+    Parameters
+    ----------
 
 
+    Inputs
+    ----------
+    in_file (str): Path to reoriented motion corrected functional data.
+    design_file (str): Path to matrix which contains all the nuissance regressors (motion + compcor noise + ...).
+
+    Outputs
+    ----------
+    probmap_csf (str): Path to csf probability map.
+
+    Sinking
+    ----------
+    - The probability maps for csf, gm and wm.
+
+    Acknowledgements
+    ----------
+    Adapted from Balint Kincses (2018)
+
+    """
+    from PUMI.pipelines.multimodal.image_manipulation import timecourse2png
+    import nipype.interfaces.fsl as fsl
+
+    nuisance_regression = Node(interface=fsl.FilterRegressor(filter_all=True), name='nuisance_regression')
+    wf.connect('inputspec', 'in_file', nuisance_regression, 'in_file')
+    wf.connect('inputspec', 'design_file', nuisance_regression, 'design_file')
+
+    nuisance_removal_qc = timecourse2png('nuisance_removal_qc', sink=True)
+    wf.connect(nuisance_regression, 'out_file', nuisance_removal_qc, 'func')
+
+    # sinking
+    wf.connect(nuisance_regression, 'out_file', 'sinker', 'func_nuis_corrected')
+
+    # output
+    wf.connect(nuisance_regression, 'out_file', 'outputspec', 'out_file')
 
