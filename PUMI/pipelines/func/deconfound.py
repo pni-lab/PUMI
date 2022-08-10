73e1da2f66b86be2ba63ca9cf2c817293f6aaf77
Kevin Hoffschlag <72939508+khoffschlag@users.noreply.github.com>
2022-08-10 21:27:52 +0200

    port McFlirt motion correction

--- PUMI/pipelines/func/deconfound.py
+++ PUMI/pipelines/func/deconfound.py
@@ -1,10 +1,13 @@
-from nipype.interfaces import afni
-from PUMI.engine import NestedNode as Node
+from nipype.algorithms import confounds
+from nipype.interfaces import afni, fsl, utility
+from PUMI.engine import NestedNode as Node, QcPipeline
 from PUMI.engine import FuncPipeline
 
 
 # Inputspec  is the input of the workflow
 # Outputspec is the output of the workflow
+from PUMI.pipelines.multimodal.image_manipulation import pick_volume, timecourse2png
+from PUMI.utils import calc_friston_twenty_four, calculate_FD_Jenkinson, mean_from_txt, max_from_txt
 
 
 @FuncPipeline(inputspec_fields=['in_file'],
@@ -20,6 +23,198 @@ def despiking_afni(wf, **kwargs):
 
     #todo: qc
 
+
+@QcPipeline(inputspec_fields=['func', 'motion_correction', 'plot_motion_trans', 'FD_figure'],
+            outputspec_fields=[],
+            default_regexp_sub=False,
+            regexp_sub=[(r'(.*\/)([^\/]+)\/([^\/]+)\/([^\/]+)$', r'\g<1>qc_motion_correction/\g<3>-\g<2>.png'),
+                        ('_subject_', 'sub-')])
+def qc_motion_correction_mcflirt(wf, **kwargs):
+    """
+
+    Save quality check images for mcflirt motion-correction
+
+    Inputs
+    ----------
+    func (str):
+    motion_correction (str):
+    plot_motion_trans (str):
+    FD_figure (str):
+
+    Outputs
+    ----------
+
+    Sinking
+    ----------
+    - rotations plot
+    - translations plot
+    - FD plot
+    - timeseries
+
+    """
+
+    mc_timecourse = timecourse2png('mc_timeseries', sink=False)  # sink=False important for qc-folder-struktur
+    wf.connect('inputspec', 'func', mc_timecourse, 'func')
+
+    # sinking
+    wf.connect(mc_timecourse, 'out_file', 'sinker', 'mc_timeseries')
+    wf.connect('inputspec', 'motion_correction', 'sinker', 'mc_rotations')
+    wf.connect('inputspec', 'plot_motion_trans', 'sinker', 'mc_translations')
+    wf.connect('inputspec', 'FD_figure', 'sinker', 'FD')
+
+
+@FuncPipeline(inputspec_fields=['in_file'],
+              outputspec_fields=['func_out_file', 'mat_file', 'mc_par_file', 'friston24_file', 'FD_file'])
+def motion_correction_mcflirt(wf, reference_vol='middle', FD_mode='Power', **kwargs):
+    """
+
+    Use FSL MCFLIRT to do the motion correction of the 4D functional data and use the 6df rigid body motion parameters
+    to calculate friston24 parameters for later nuissance regression step.
+
+    Parameters
+    ----------
+    reference_vol (str): Either "first", "middle", "last", "mean", or the index of the volume which the rigid body
+                         registration (motion correction) will use as reference.
+                         Default is 'middle'.
+    FD_mode: Either "Power" or "Jenkinson"
+
+    Inputs
+    ----------
+    in_file (str): Reoriented functional file
+
+    Outputs
+    ----------
+    func_out_file (str): Path to motion-corrected timeseries
+    mat_file (str): Path to motion-correction transformation matrices
+    mc_par_file (str): Path to file with motion parameters
+    friston24_file (str): Path to file with friston24 parameters
+    FD_file (str): Path to file with FD
+
+    Sinking
+    ----------
+    - motion-corrected timeseries
+    - motion-correction transformation matrices
+    - absolute and relative displacement parameters
+    - friston24 parameters
+    - FD
+    - FDmax
+    - quality check images (FD/rotations/translations and timeseries plot)
+
+    Acknowledgements
+    ----------
+    Adapted from Balint Kincses (2018)
+
+    Modified version of PAC.func_preproc.func_preproc
+    (https://github.com/FCP-INDI/C-PAC/blob/main/CPAC/func_preproc/func_preproc.py)
+    and CPAC.generate_motion_statistics.generate_motion_statistics
+    (https://github.com/FCP-INDI/C-PAC/blob/main/CPAC/generate_motion_statistics/generate_motion_statistics.py)
+    """
+
+    if FD_mode not in ['Power', 'Jenkinson']:
+        raise ValueError(f'FD_mode has to be "Power" or "Jenkinson"! %s is not a valid option!' % FD_mode)
+
+    refvol = pick_volume(volume=reference_vol, name='refvol')
+    wf.connect('inputspec', 'in_file', refvol, 'in_file')
+
+    mcflirt = Node(interface=fsl.MCFLIRT(interpolation="spline", stats_imgs=False), name='mcflirt')
+    if reference_vol == "mean":
+        mcflirt.inputs.mean_vol = True
+    mcflirt.inputs.dof = 6
+    mcflirt.inputs.save_mats = True
+    mcflirt.inputs.save_plots = True
+    mcflirt.inputs.save_rms = True
+    mcflirt.inputs.stats_imgs = False
+    wf.connect('inputspec', 'in_file', mcflirt, 'in_file')
+    if reference_vol != "mean":
+        wf.connect(refvol, 'out_file', mcflirt, 'ref_file')
+
+    calc_friston = Node(
+        utility.Function(
+            input_names=['in_file'], output_names=['out_file'],
+            function=calc_friston_twenty_four
+        ),
+        name='calc_friston'
+    )
+    wf.connect(mcflirt, 'par_file', calc_friston, 'in_file')
+
+    if FD_mode == "Power":
+        calculate_FD = Node(
+            confounds.FramewiseDisplacement(
+                parameter_source='FSL',
+                save_plot=True,
+                out_figure='fd_power_2012.png'
+            ),
+            name='calculate_FD_Power'
+        )
+    elif FD_mode == "Jenkinson":
+        calculate_FD = Node(
+            utility.Function(
+                input_names=['in_file'],
+                output_names=['out_file'],
+                function=calculate_FD_Jenkinson
+            ),
+            name='calculate_FD_Jenkinson'
+        )
+    wf.connect(mcflirt, 'par_file', calculate_FD, 'in_file')
+
+    mean_FD = Node(
+        utility.Function(
+            input_names=['in_file', 'axis', 'header', 'out_file'],
+            output_names=['mean_file'],
+            function=mean_from_txt
+        ),
+        name='meanFD'
+    )
+    mean_FD.inputs.axis = 0  # global mean
+    mean_FD.inputs.header = True  # global mean
+    mean_FD.inputs.out_file = 'FD.txt'
+    wf.connect(calculate_FD, 'out_file', mean_FD, 'in_file')
+
+    max_FD = Node(
+        utility.Function(
+            input_names=['in_file', 'axis', 'header', 'out_file'],
+            output_names=['max_file'],
+            function=max_from_txt
+        ),
+        name='maxFD'
+    )
+    max_FD.inputs.axis = 0  # global mean
+    max_FD.inputs.header = True
+    max_FD.inputs.out_file = 'FDmax.txt'
+    wf.connect(calculate_FD, 'out_file', max_FD, 'in_file')
+
+    plot_motion_rot = Node(
+        interface=fsl.PlotMotionParams(in_source='fsl'),
+        name='plot_motion_rot')
+    plot_motion_rot.inputs.plot_type = 'rotations'
+    wf.connect(mcflirt, 'par_file', plot_motion_rot, 'in_file')
+
+    plot_motion_trans = Node(
+        interface=fsl.PlotMotionParams(in_source='fsl'),
+        name='plot_motion_trans')
+    plot_motion_trans.inputs.plot_type = 'translations'
+    wf.connect(mcflirt, 'par_file', plot_motion_trans, 'in_file')
+
+    qc_mc = qc_motion_correction_mcflirt('qc_mc')
+    wf.connect(plot_motion_rot, 'out_file', qc_mc, 'motion_correction')
+    wf.connect(plot_motion_trans, 'out_file', qc_mc, 'plot_motion_trans')
+    wf.connect(calculate_FD, 'out_figure', qc_mc, 'FD_figure')
+    wf.connect(mcflirt, 'out_file', qc_mc, 'func')
+
+    # sinking
+    wf.connect(mcflirt, 'out_file', 'sinker', 'mc_func')
+    wf.connect(mcflirt, 'par_file', 'sinker', 'mc_par')
+    wf.connect(mcflirt, 'rms_files', 'sinker', 'mc_rms')
+    wf.connect(calc_friston, 'out_file', 'sinker', 'mc_first24')
+    wf.connect(mean_FD, 'mean_file', 'sinker', 'FD')
+    wf.connect(max_FD, 'max_file', 'sinker', 'FDmax')
+
+    # output
+    wf.connect(mcflirt, 'out_file', 'outputspec', 'func_out_file')
+    wf.connect(mcflirt, 'mat_file', 'outputspec', 'mat_file')
+    wf.connect(mcflirt, 'par_file', 'outputspec', 'mc_par_file')
+    wf.connect(calculate_FD, 'out_file', 'outputspec', 'FD_file')
+    wf.connect(calc_friston, 'out_file', 'outputspec', 'friston24_file')
 
 
 
--- PUMI/utils.py
+++ PUMI/utils.py
@@ -391,3 +391,165 @@ def drop_first_line(in_file):
     with open(fname, 'w') as fout:
         fout.writelines(data[1:])  # don't write the first line into the new file
         return os.path.join(os.getcwd(), fname)
+
+
+def calc_friston_twenty_four(in_file):
+    """
+
+    Method to calculate friston twenty four parameters
+
+    Parameters
+    ----------
+    in_file (str): input movement parameters file from motion correction
+
+    Returns
+    ----------
+    new_file (str): output 1D file containing 24 parameter values
+
+    """
+
+    import numpy as np
+    import os
+
+    data = np.genfromtxt(in_file)
+    data_squared = data ** 2
+    new_data = np.concatenate((data, data_squared), axis=1)
+    data_roll = np.roll(data, 1, axis=0)
+    data_roll[0] = 0
+    new_data = np.concatenate((new_data, data_roll), axis=1)
+    data_roll_squared = data_roll ** 2
+    new_data = np.concatenate((new_data, data_roll_squared), axis=1)
+    new_file = os.path.join(os.getcwd(), 'fristons_twenty_four.1D')
+    np.savetxt(new_file, new_data, delimiter=' ')
+    return new_file
+
+
+def calculate_FD_Jenkinson(in_file):
+    """
+
+    Method to calculate friston twenty four parameters
+
+    Parameters
+    ----------
+    in_file (str): input movement parameters file from motion correction
+
+    Returns
+    ----------
+    new_file (str): path to output file
+
+    """
+
+    import numpy as np
+    import os
+    import math
+
+    out_file = os.path.join(os.getcwd(), 'FD_J.1D')
+
+    lines = open(in_file, 'r').readlines()
+    rows = [[float(x) for x in line.split()] for line in lines]
+    cols = np.array([list(col) for col in zip(*rows)])
+
+    translations = np.transpose(np.diff(cols[3:6, :]))
+    rotations = np.transpose(np.diff(cols[0:3, :]))
+
+    flag = 0
+    rmax = 80.0  # The default radius (as in FSL) of a sphere represents the brain
+
+    out_lines = []
+
+    for i in range(0, translations.shape[0] + 1):
+
+        if flag == 0:
+            flag = 1
+            # first timepoint
+            out_lines.append('0')
+        else:
+            r = rotations[i - 1,]
+            t = translations[i - 1,]
+            FD_J = math.sqrt((rmax * rmax / 5) * np.dot(r, r) + np.dot(t, t))
+            out_lines.append('\n{0:.8f}'.format(FD_J))
+
+    with open(out_file, "w") as f:
+        for line in out_lines:
+            f.write(line)
+    return out_file
+
+
+def mean_from_txt(in_file, axis=None, header=False, out_file='mean.txt'):
+    """
+
+    Calculate column-means, row-means or the global mean depending on the 'axis' parameter and save it to
+    another text-file.
+
+    Caution: Name in the old PUMI was txt2MeanTxt
+
+    Parameters
+    ----------
+    in_file (str): input file
+    axis (None/int/(int,int)): axis or axes along which the means are computed.
+                               Default: Compute mean of the flattened array.
+    header (bool): Drop line if True
+    out_file (str): Name of the resulting text-file. If only the filename is given, it will be saved into the
+                    current working directory.
+
+    Returns
+    ----------
+    new_file (str): path to new file
+
+    """
+    import numpy as np
+    import os
+
+    if header:
+        print("drop first line")
+        data = np.loadtxt(in_file, skiprows=1)
+    else:
+        print("don't drop first line")
+        data = np.loadtxt(in_file)
+    mean = data.mean(axis=axis)
+    np.savetxt(out_file, [mean])
+    if out_file.startswith('/'):
+        new_file = out_file
+    else:
+        new_file = os.path.join(os.getcwd(), out_file)  # need to add '/' manually?
+    return new_file
+
+
+def max_from_txt(in_file, axis=None, header=False, out_file='max.txt'):
+    """
+
+    Saves the maximum along a given axis into another text-file.
+
+    Caution: Name in the old PUMI was txt2MaxTxt
+
+    Parameters
+    ----------
+    in_file (str): input file
+    axis (None/int/(int,int)): axis or axes along the maximum values are computed.
+                               Default: Use flattened array.
+    header (bool): Drop line if True
+    out_file (str): Name of the resulting text-file. If only the filename is given, it will be saved into the
+                    current working directory.
+
+    Returns
+    ----------
+
+    new_file (str): path to new file
+
+    """
+    import numpy as np
+    import os
+
+    if header:
+        print("drop first line")
+        data = np.loadtxt(in_file, skiprows=1)
+    else:
+        print("don't drop first line")
+        data = np.loadtxt(in_file)
+    dmax = data.max(axis=axis)
+    np.savetxt(out_file, [dmax])
+    if out_file.startswith('/'):
+        new_file = out_file
+    else:
+        new_file = os.path.join(os.getcwd(), out_file)  # need to add '/' manually?
+    return new_file
