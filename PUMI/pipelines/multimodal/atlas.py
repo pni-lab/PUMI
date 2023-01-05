f1bb25ecf407dff5764d922db037958d4244a17c
mberjano <berjanomaria@gmail.com>
2023-01-05 20:52:55 +0100

    atlas testing

--- PUMI/pipelines/multimodal/atlas.py
+++ PUMI/pipelines/multimodal/atlas.py
@@ -3,25 +3,22 @@ from nilearn.datasets import atlas as nlatlas
 from fnmatch import fnmatch
 import numpy as np
 import pandas as pd
-from PUMI.utils import mist_labels
+import nibabel as nib
 import os
 
-#@GroupPipeline(inputspec_fields=['atlas_query'],
- #             outputspec_fields=['labels', 'labelmap'])
-def fetch_atlas(atlas_query, atlas_dir=None):
+def fetch_atlas(atlasname, atlas_dir=None, **kwargs):
     """
-    Determines which atlas to fetch and what version of the atlas to use (if applicable).
+    Determines which atlas to fetch.
 
         Parameters
         ----------
-        atlas_query : str or list
-            Input string in the following format:
-            [atlas_name,atlas_parameters]. The following can be for
-            `atlas_name`: 'aal,'destrieux','difumo','harvard_oxford','juelich','msdl','MIST'
-                          'pauli','schaefer','talairach','yeo'
-            `atlas_parameters` is not available for the `destrieux` atlas.
+        atlasname : str or list
+            Input string can be for: 'aal,'destrieux','difumo','harvard_oxford','juelich','msdl',
+                                     'MIST','pauli','schaefer','talairach','yeo'
         atlas_dir : str, optional
             Path to a custom atlas labelmap and label text file.
+
+        Note: **kwargs is not available for the `destrieux` atlas.
 
         Returns
         -------
@@ -34,54 +31,61 @@ def fetch_atlas(atlas_query, atlas_dir=None):
             Raised when the atlas is not formatted correctly or if there is no
             match found.
         """
-    # extract parameters
-    if len(atlas_query) >= 2:
-        atlas_name = atlas_query[0]
-        sub_param = atlas_query[1:]
-    elif len(atlas_query) == 1:
-        atlas_name = atlas_query[0]
-        sub_param = None
-    else:
-        raise ValueError('Incorrect query string provided')
 
     # get atlas
     if not atlas_dir:
-        if atlas_name == 'MIST':
-            mist_dir = os.path.abspath(os.path.join(os.path.expanduser(os.path.dirname(os.path.dirname(__file__))), "../data_in/atlas/MIST"))
-            resolution = sub_param[0]
-            labelmap = mist_dir + '/Parcellations/MIST_' + resolution + '.nii.gz'
-            labels = mist_labels(mist_dir,resolution=resolution)
+        if atlasname == 'MIST':
+            mist_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__)), "../data_in/atlas/MIST"))
+            labelmap_dir = mist_dir + '/Parcellations/MIST_' + kwargs['resolution'] + '.nii.gz'
+            labelmap = nib.load(labelmap_dir)
+            labels_dir = mist_dir + '/Parcel_Information/MIST_' + kwargs['resolution'] + '.csv'
+            labels = pd.read_csv(labels_dir, delimiter=";")
+            labels = labels['label'].values.tolist()
         else:
-            function_name = [x for x in dir(nlatlas) if fnmatch(x, "fetch_atlas_" + atlas_name + "*")]
+            function_name = [x for x in dir(nlatlas) if fnmatch(x, "fetch_atlas_" + atlasname + "*")]
             if function_name:
                 function_name = str(function_name[0])
                 atlas_function = getattr(nlatlas, function_name)
-                if sub_param and atlas_name != 'yeo':
-                    atlas = atlas_function(*sub_param)
+                if atlasname in {'destrieux','difumo'}:
+                    kwargs = {'legacy_format':False}
+
+                if kwargs and atlasname != 'yeo':
+                    atlas = atlas_function(**kwargs)
                 else:
                     atlas = atlas_function()
 
-                if atlas_name == 'yeo':
-                    labelmap = atlas[sub_param[0]]
-                    if '17' in sub_param:
+                if atlasname == 'yeo':
+                    labelmap = atlas[kwargs['key']]
+                    if '17' in kwargs['key']:
                         labels = pd.read_csv(atlas['colors_17'], sep=r'\s+')['NONE'].tolist()
                     else:
                         labels = pd.read_csv(atlas['colors_7'], sep=r'\s+')['NONE'].tolist()
                 else:
                     labelmap = atlas['maps']
                     labels = atlas['labels']
-                    # labels = labels.astype(str).tolist()
-                    if atlas_name == 'schaefer':
+                    if atlasname == 'schaefer':
                         labels = np.insert(atlas['labels'], 0, 'Background')
             else:
                 raise ValueError('No atlas detected. Check query string')
     else:
         if os.path.exists(atlas_dir):
-            labelmap = atlas_dir + '/Parcellations/' + sub_param[0] + '.nii.gz'
-            labels_file = atlas_dir + '/Parcel_information/' + sub_param[1] + '.csv'
+            labelmap = atlas_dir + '/Parcellations/' + atlasname + '.nii.gz'
+            labels_file = atlas_dir + '/Parcel_information/' + atlasname + '.csv'
             labels = pd.read_csv(labels_file, sep=";")
             labels = labels.values.tolist()
         else:
             raise ValueError('No atlas detected. Check query or atlas directory')
+
+    labels_out = os.path.join(os.getcwd(), atlasname + 'newlabels.tsv')
+    if isinstance(labels, pd.DataFrame):
+        labels = labels.values.tolist()
+
+    df = pd.DataFrame({'Labels': labels})
+    if atlasname == 'aal':
+        df.index = atlas['indices'] # corresponding to region IDs
+    else:
+        df.index += 1  # indexing from 1
+    df.to_csv(labels_out,sep='\t')
+    print(df.head())
 
     return labelmap, labels
