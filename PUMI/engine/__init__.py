f9f2f2ba4640c376b7d28c189af9c7ae1a4f8403
Kevin Hoffschlag <72939508+khoffschlag@users.noreply.github.com>
2025-04-26 13:45:54 +0200

    Introduce engine/pipelines

--- PUMI/engine/__init__.py
+++ PUMI/engine/__init__.py
@@ -1,8 +1,15 @@
 from .nodes import NestedNode, NestedMapNode
 from .workflow import NestedWorkflow
+from .pipelines import AnatPipeline, FuncPipeline, GroupPipeline, QcPipeline, PumiPipeline, BidsPipeline
 
 __all__ = [
     'NestedNode',
     'NestedMapNode',
     'NestedWorkflow',
+    'AnatPipeline',
+    'FuncPipeline',
+    'GroupPipeline',
+    'QcPipeline',
+    'PumiPipeline',
+    'BidsPipeline'
 ]
