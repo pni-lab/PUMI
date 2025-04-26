0537dc3f3528f156fae07c1f1f531451322fbb85
Kevin Hoffschlag <72939508+khoffschlag@users.noreply.github.com>
2025-04-26 16:17:09 +0200

    Introduce utils/statistics

--- PUMI/utils/__init__.py
+++ PUMI/utils/__init__.py
@@ -2,6 +2,8 @@ from .config import get_config
 from .references import get_reference, get_fallback_reference, parse_reference_path, get_ref_from_templateflow
 from .plotting import plot_roi, segmentation_qc_plot, coregistration_qc_plot
 from .image_transformations import scrub_image, scale_vol, registration_ants_hardcoded
+from .statistics import max_from_txt, mean_from_txt, calc_friston_twenty_four, calculate_FD_Jenkinson
+
 
 __all__ = [
     'get_config',
@@ -14,5 +16,9 @@ __all__ = [
     'coregistration_qc_plot',
     'scrub_image',
     'scale_vol',
-    'registration_ants_hardcoded'
+    'registration_ants_hardcoded',
+    'max_from_txt',
+    'mean_from_txt',
+    'calc_friston_twenty_four',
+    'calculate_FD_Jenkinson'
 ]
