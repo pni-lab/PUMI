bfd2ed9f54e3d715089a50481d6e45eca1c79bc0
Kevin Hoffschlag <72939508+khoffschlag@users.noreply.github.com>
2025-04-26 14:17:31 +0200

    Introduce utils/plotting

--- PUMI/utils/__init__.py
+++ PUMI/utils/__init__.py
@@ -1,11 +1,12 @@
 from .config import get_config
 from .references import get_reference, get_fallback_reference, parse_reference_path, get_ref_from_templateflow
-
+from .plotting import plot_roi
 
 __all__ = [
     'get_config',
     'get_reference',
     'get_fallback_reference',
     'parse_reference_path',
-    'get_ref_from_templateflow'
+    'get_ref_from_templateflow',
+    'plot_roi'
 ]
