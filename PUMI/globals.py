28d356cf6d735b86fe6c970ac0376c3348f56bf9
Kevin Hoffschlag <72939508+khoffschlag@users.noreply.github.com>
2025-04-26 18:41:50 +0200

    Move from SafeConfigParser to ConfigParser due to SafeConfigPaser being deprecated for Python >= 12

--- PUMI/globals.py
+++ PUMI/globals.py
@@ -1,7 +1,7 @@
-from configparser import SafeConfigParser
+from configparser import ConfigParser
 import PUMI
 import os
 
-cfg_parser = SafeConfigParser()
+cfg_parser = ConfigParser()
 cfg_parser.optionxform = str
 cfg_parser.read(os.path.join(os.path.dirname(PUMI.__file__), 'settings.ini'))
